4b47ce0c2f3e0f3c878c80ee1abf45dbd2a16047
JaydenDev <willsjayden2@gmail.com>
2022-04-25 17:48:34 -0500

    New Website

---
+++ javascript/global.js
@@ -0,0 +1,10 @@
+// JaydenDev 2022
+// MIT License, Website Source
+
+setInterval(function() {
+    document.getElementById("title").classList.add("text-emerald-500");
+}, 1000);
+
+setInterval(function() {
+    document.getElementById("title").classList.remove("text-emerald-500");
+}, 2000);
