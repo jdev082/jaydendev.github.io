f4d9a6afd4fa366d8976be896bdce5232193aac3
JaydenDev <willsjayden2@gmail.com>
2022-05-26 10:27:33 -0500

    Fix 404 (2)

--- error/javascript/global.js
+++
@@ -1,7 +0,0 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-
-let error = "404";
-if (error == '404') {
-    document.querySelector('#errCode').innerText = "Invalid or missing page."
-}
